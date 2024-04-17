4bb3c1a90f6180e4cbac315c3834b69c06ea24f8
Alambek Gulamidinov <alam@nepla.ai>
2024-04-17 09:13:28 +0900

    Add login page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-/**
- * Entry point of application, where App is rendered within the div with the id of "app" 
- */
-
 import React from "react";
-import { render } from "react-dom";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
 
 import App from "./App";
+import Login from "./loginPage/Login"
+import HomePage from "./loginPage/HomePage"
 
-render(<App></App>, document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/HomePage" element={<HomePage />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/loginPage/Login.jsx
@@ -0,0 +1,68 @@
+import './Login.css';
+import { useNavigate } from "react-router-dom";
+import { useRef, useState } from 'react';
+
+function Login() {
+  const navigate = useNavigate();
+  const popUp = useRef('null');
+  const [visOne, setVisOne] = useState('');
+  const [visTwo, setVisTwo] = useState('');
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget)
+    const username = formData.get('username')
+    const pwd = formData.get('password')
+
+    console.log(username, pwd)
+    if(pwd === "test1234!" && username === "test@luxpmsoft.com"){
+      alert("success!")
+      navigate('/HomePage');
+    }
+    else{
+      popUp.current.showModal();
+    }
+  }
+  const handleChange = (event, type) => {
+    const match = event.target.value.match(/^[!@#$%^&*(),.?":{}|<>a-zA-Z0-9]+$/);
+    if(!match){
+      (type == 1)? setVisOne("notVis") : setVisTwo("notVis")
+    }
+    else{
+      (type == 1)? setVisOne("") : setVisTwo("")
+    }
+  }
+
+  return (
+    <div className="App">
+
+      <dialog ref={popUp} >
+        <p>the provided password is wrong!</p>
+        <button onClick={() => popUp.current.close()}>Close</button>
+      </dialog>
+
+      <div className='login-container'>
+        <form className='login-form' onSubmit={handleSubmit}>
+          <img src="../images/loginImg/remove.png" className='cart'/>
+          <div className='input_field'>
+            <img src="../images/loginImg/user.png" className='user' />
+            <input type="text" name="username" placeholder='username' onChange={(e) => handleChange(e, 1)}/>
+          </div>
+          <p className={`alertMessage ${visOne}`}>Wrong combination!</p>
+
+          <div className='input_field'>
+            <img src="../images/loginImg/lock.png" className='lock' />
+            <input type="password" name="password" placeholder='password' onChange={(e) => handleChange(e, 2)}/>
+          </div>
+          <p className={`alertMessage ${visTwo}`}>Wrong combination!</p>
+
+          <button type="submit" class="loginBtn"> Login</button>
+          <p className='forgotBtn' type="button">Forgot password?</p>
+
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
